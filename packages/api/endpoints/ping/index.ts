937222159c6f24ce8109ee3335d1cdbf2ee33d43
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-08 10:09:06 +0200

    add deps

--- packages/api/endpoints/ping/index.ts
+++ packages/api/endpoints/ping/index.ts
@@ -3,7 +3,7 @@ import { NowRequest, NowResponse } from '@now/node';
 import sendSuccess from '../../utils/send-success';
 import corsHeaders from '../../utils/cors-headers';
 
-module.exports = async function(req: NowRequest, res: NowResponse) {
+export default async function(req: NowRequest, res: NowResponse) {
   // Set CORS Headers & content-type
   corsHeaders(res);
   res.setHeader('Content-Type', 'application/json');
