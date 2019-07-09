48fb7535d3ca6b326520af381218f0bd2bc02678
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-09 14:45:37 +0200

    fix frontend stuff

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import { withRouter } from 'next/router';
 
+import '../utils/setup-axios';
+
 export type Props = {
   pong: null | {
     data: string;
@@ -13,7 +15,7 @@ export type Props = {
 class Index extends React.Component<Props> {
   static async getInitialProps(): Promise<Props> {
     try {
-      let pong = await axios.get('/api/ping');
+      let pong = await axios.get('/ping');
 
       return { pong: pong.data };
     } catch (e) {
---
+++ packages/frontend/utils/setup-axios.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://voices-to-emotions.osoc19.now.sh/api';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
