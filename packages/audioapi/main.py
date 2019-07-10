1504eec4d068bffe021908af36529847253094a8
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-10 20:08:31 +0200

    AI API

--- packages/audioapi/main.py
+++ packages/audioapi/main.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 from werkzeug.utils import secure_filename
 import librosa, os, math, tempfile, json, gc, base64
 import numpy as np
+from predict_emotions import predict_emotions
 
 app = Flask(__name__)
 
@@ -10,21 +11,46 @@ temp_dir = tempfile.gettempdir()
 mfcc_features = 20
 
 
-def base64jsonify(data):
+def base64JsonResponse(data):
   data = json.dumps(data, separators=(',', ':'))
   return Response(base64.b64encode(data.encode('utf-8')), mimetype='text/plain')
 
 
+def jsonResponse(data):
+  return Response(json.dumps(data, separators=(',', ':')), mimetype='application/json')
+
+
+def load_audio_data(file_path):
+  # 16000 Hz = VoIP
+  wave, sr = librosa.load(file_path, mono=True, sr=16000)
+  wave_frag_offsets = librosa.effects.split(wave, top_db=35)
+
+  results = []
+  for offsets in wave_frag_offsets:
+    wave_fragment = wave[offsets[0]:offsets[1]]
+    mfcc = librosa.feature.mfcc(wave_fragment, sr, n_mfcc=mfcc_features)
+    _, y_size = mfcc.shape
+
+    splitted_mfcc = np.array_split(mfcc, math.ceil(y_size / 500), axis=1)
+
+    for short_mfcc in splitted_mfcc:
+      short_mfcc = np.pad(short_mfcc, ((0, 0), (0, 500 - len(short_mfcc[0]))), mode='constant', constant_values=0)
+      results.append(np.array(short_mfcc).tolist())
+
+  return results
+
+
 @app.route('/', methods=['POST'])
 def upload():
-  try:
-    if request.method == 'POST':
-      f = request.files['audio']
+  if request.method == 'POST':
+    f = request.files['audio']
 
-      if f and f.filename:
-        filename = secure_filename(f.filename)
-        extname = filename.rsplit('.', 1)[1].lower()
-        target_path = os.path.join(temp_dir, 'temp.' + extname)
+    if f and f.filename:
+      filename = secure_filename(f.filename)
+      extname = filename.rsplit('.', 1)[1].lower()
+      target_path = os.path.join(temp_dir, filename)
+
+      if extname in ALLOWED_EXTENSIONS:
         f.save(target_path)
         f.close()
 
@@ -32,49 +58,38 @@ def upload():
         del f
         gc.collect()
 
-        if extname in ALLOWED_EXTENSIONS:
-          wave, sr = librosa.load(target_path, sr=16000, mono=True)
-          wave_frag_offsets = librosa.effects.split(wave, top_db=35)
+        mfcc = load_audio_data(target_path)
 
-          results = []
-          for offsets in wave_frag_offsets:
-            wave_fragment = wave[offsets[0]:offsets[1]]
-            mfcc = librosa.feature.mfcc(wave_fragment, sr, n_mfcc=mfcc_features)
-            _, y_size = mfcc.shape
+        predicted_emotions = []
+        if (len(mfcc) > 0):
+          predicted_emotions = predict_emotions(mfcc)
 
-            splitted_mfcc = np.array_split(mfcc, math.ceil(y_size / 500), axis=1)
+        os.remove(target_path)
 
-            for short_mfcc in splitted_mfcc:
-              short_mfcc = np.pad(short_mfcc, ((0, 0), (0, 500 - len(short_mfcc[0]))), mode='constant',
-                                  constant_values=0)
-              results.append(np.array(short_mfcc).tolist())
-
-          return base64jsonify({
-            "type": 'success',
-            "data": results
-          })
-        else:
-          return base64jsonify({
-            "type": 'error',
-            "message": "Invalid filetype"
-          })
-      else:
-        return base64jsonify({
-          "type": 'error',
-          "message": "Please provide a file"
+        return jsonResponse({
+          "type": 'success',
+          "data": {
+            'emotions': predicted_emotions,
+            # 'mfcc': mfcc
+          }
         })
 
-
+      else:
+        return jsonResponse({
+          "type": 'error',
+          "message": "Invalid filetype"
+        })
 
     else:
-      return base64jsonify({
+      return jsonResponse({
         "type": 'error',
-        "message": "Unknown request"
+        "message": "Please provide a file"
       })
-  except:
-    return base64jsonify({
+
+  else:
+    return jsonResponse({
       "type": 'error',
-      "message": "Unknown error"
+      "message": "Unknown request"
     })
 
 
---
+++ packages/audioapi/predict_emotions.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+
+
+def normaliseFloat(n):
+  return round(n * 10000) / 10000
+
+
+def predict_emotions(mfcc_data):
+  with tf.gfile.FastGFile('model/emotions.pb', 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+  with tf.Session() as sess:
+    input_tensor = sess.graph.get_tensor_by_name('FullyConnected/Softmax:0')
+    results = sess.run(input_tensor, {
+      'InputData/X:0': mfcc_data
+    })
+
+    mapped_results = []
+    for p in results:
+      mapped_results.append({
+        'neutral': normaliseFloat(p[0]),
+        'calm': normaliseFloat(p[1]),
+        'happy': normaliseFloat(p[2]),
+        'sad': normaliseFloat(p[3]),
+        'angry': normaliseFloat(p[4]),
+        'fearful': normaliseFloat(p[5]),
+        'disgust': normaliseFloat(p[6]),
+        'surprised': normaliseFloat(p[7])
+      })
+
+    return mapped_results
