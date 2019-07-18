e91cd2a45f6fc2809d8b981abef2196d1bcf3a82
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-18 14:46:01 +0200

    .

--- packages/frontend/utils/setup-axios.ts
+++ packages/frontend/utils/setup-axios.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://voices-to-emotions.osoc19.now.sh/api';
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://voices-to-emotions.osoc19.now.sh/api';
+// axios.defaults.baseURL = 'http://localhost:3000/api';
