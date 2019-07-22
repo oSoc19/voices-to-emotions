8bdf0b5434371a75bc5871ff53656b136efb873a
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-22 15:33:32 +0200

    cleanup graph layour

--- packages/frontend/components/user-graphs.tsx
+++ packages/frontend/components/user-graphs.tsx
@@ -12,17 +12,10 @@ export type Props = {
 };
 
 const GraphContainer = styled.div`
-  display: grid;
   box-sizing: border-box;
   padding: 20px 0;
-  grid-template-columns: 1fr 2fr;
   grid-gap: 20px;
   width: 100%;
-
-  @media (max-width: 960px) {
-    grid-template-rows: repeat(3, auto 1fr);
-    grid-template-columns: 1fr;
-  }
 `;
 
 const GraphInformation = styled.div`
@@ -30,6 +23,12 @@ const GraphInformation = styled.div`
   width: 100%;
   box-sizing: border-box;
   padding: 20px 20px 20px 0;
+
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-areas: 'heading heading' 'text graph';
+  margin-bottom: 10px;
+  grid-gap: 10px;
 `;
 
 const commaToPercentage = commaValue => {
@@ -96,28 +95,30 @@ export default function(props: Props) {
   return (
     <GraphContainer>
       <GraphInformation>
-        <SubHeading style={{ fontSize: '1.5rem' }}>Emotions</SubHeading>
+        <SubHeading style={{ fontSize: '1.5rem', gridArea: 'heading' }}>Emotions</SubHeading>
+
         <Paragraph>
           This graph shows an overview of the emotions recorded in each call. The green line indicates the percentage of
           fragments that are classified as a positive emotion, namely happy. The red line indicates the percentage of
           fragments that are classified as a negative emotion, namely angry, sad, or fearful. Each call is a time point.
         </Paragraph>
+
+        <ResponsiveContainer width="100%" height={250}>
+          <LineChart height={250} data={emotionsData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="datetime" />
+            <YAxis label={{ value: 'Amount of Emotion', angle: -90, position: 'insideBottomLeft', offset: 10 }} />
+            <Tooltip content={TooltipRenderer('%')} />
+            <Legend align="right" verticalAlign="top" />
+            <Line type="monotone" dataKey="Positive Emotions" stroke="#45a06f" />
+            <Line type="monotone" dataKey="Negative Emotions" stroke="#f44336" />
+          </LineChart>
+        </ResponsiveContainer>
       </GraphInformation>
 
-      <ResponsiveContainer width="100%" height={250}>
-        <LineChart height={250} data={emotionsData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="datetime" />
-          <YAxis label={{ value: 'Amount of Emotion', angle: -90, position: 'insideBottomLeft', offset: 10 }} />
-          <Tooltip content={TooltipRenderer('%')} />
-          <Legend align="right" verticalAlign="top" />
-          <Line type="monotone" dataKey="Positive Emotions" stroke="#45a06f" />
-          <Line type="monotone" dataKey="Negative Emotions" stroke="#f44336" />
-        </LineChart>
-      </ResponsiveContainer>
-
       <GraphInformation>
-        <SubHeading style={{ fontSize: '1.5rem' }}>Duration</SubHeading>
+        <SubHeading style={{ fontSize: '1.5rem', gridArea: 'heading' }}>Duration</SubHeading>
+
         <Paragraph>
           This graph shows an overview of the length of each call, in seconds. Longer calls than normally might indicate
           that the employee is less attentive or motivated. Shorter calls than normally might indicate that the employee
@@ -125,39 +126,40 @@ export default function(props: Props) {
           length from the average of the last 10 calls for this specific employee, and thus both longer and shorter
           calls than normally increase how likely this employee would quit.
         </Paragraph>
+
+        <ResponsiveContainer width="100%" height={250}>
+          <LineChart height={250} data={durationData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="datetime" />
+            <YAxis label={{ value: 'Call Duration', angle: -90, position: 'insideBottomLeft', offset: 10 }} />
+            <Tooltip content={TooltipRenderer('secs')} />
+            <Legend align="right" verticalAlign="top" />
+            <Line type="monotone" dataKey="Duration" stroke="#019de9" />
+          </LineChart>
+        </ResponsiveContainer>
       </GraphInformation>
 
-      <ResponsiveContainer width="100%" height={250}>
-        <LineChart height={250} data={durationData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="datetime" />
-          <YAxis label={{ value: 'Call Duration', angle: -90, position: 'insideBottomLeft', offset: 10 }} />
-          <Tooltip content={TooltipRenderer('secs')} />
-          <Legend align="right" verticalAlign="top" />
-          <Line type="monotone" dataKey="Duration" stroke="#019de9" />
-        </LineChart>
-      </ResponsiveContainer>
-
       <GraphInformation>
-        <SubHeading style={{ fontSize: '1.5rem' }}>Customer Satisfaction</SubHeading>
+        <SubHeading style={{ fontSize: '1.5rem', gridArea: 'heading' }}>Customer Satisfaction</SubHeading>
+
         <Paragraph>
           This graph shows an overview of feedback the customer gives the call. After the conversation, they get an
           e-mail to rate the call out of 100, based on how satisfied they are with the service provided. Customer
           satisfaction is not incorporated in the model, but is displayed here to inspect the other variables per call
           within their context. NOTE: for now, this is simulated data.
         </Paragraph>
-      </GraphInformation>
 
-      <ResponsiveContainer width="100%" height={250}>
-        <LineChart height={250} data={feedbackData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="datetime" />
-          <YAxis label={{ value: 'Customer Satisfaction', angle: -90, position: 'insideBottomLeft', offset: 10 }} />
-          <Tooltip content={TooltipRenderer('%')} />
-          <Legend align="right" verticalAlign="top" />
-          <Line type="monotone" dataKey="Customer Satisfaction" stroke="#019de9" />
-        </LineChart>
-      </ResponsiveContainer>
+        <ResponsiveContainer width="100%" height={250}>
+          <LineChart height={250} data={feedbackData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="datetime" />
+            <YAxis label={{ value: 'Customer Satisfaction', angle: -90, position: 'insideBottomLeft', offset: 10 }} />
+            <Tooltip content={TooltipRenderer('%')} />
+            <Legend align="right" verticalAlign="top" />
+            <Line type="monotone" dataKey="Customer Satisfaction" stroke="#019de9" />
+          </LineChart>
+        </ResponsiveContainer>
+      </GraphInformation>
     </GraphContainer>
   );
 }
