bda86afe666423bc75ecee36eddb6a0c6a3da2af
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-23 14:29:14 +0200

    fix list

--- packages/frontend/components/leave-card.tsx
+++ packages/frontend/components/leave-card.tsx
@@ -9,10 +9,20 @@ const Card = styled.div`
   display: grid;
   grid-template-columns: 150px 1fr;
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-  
+
   @media (max-width: 960px) {
     grid-template-columns: 1fr;
   }
+`;
+
+const UL = styled.ul`
+  margin: 0 0 0 20px;
+  padding: 0;
+`;
+
+const LI = styled.li`
+  margin: 0 0 10px 0;
+  padding: 0;
 `;
 
 export type Props = {
@@ -20,7 +30,7 @@ export type Props = {
   user: any;
 };
 
-export default function (props: Props) {
+export default function(props: Props) {
   let { leavePercentage, user } = props;
 
   let percentageStyle = {
@@ -32,43 +42,55 @@ export default function (props: Props) {
     const likeliness = props.likeliness;
     if (likeliness < 45) {
       return (
-        <Paragraph>
-          Suggestions
-          <ul>
-            <li>Schedule quarterly evaluations, instead of annual. This avoids frustrations to build up over time. When an employee is heard, they feel validated, which
-          in turn leads to higher performance.</li>
-            <li>Organise remain-calls, instead of only exit-calls. Ask the employee what
-            motivates them to stay in the company, and what it would take for them to leave. This uncoves issues before
-          they become bigger problems, and allows for a focused strategy to support the employee.</li>
-          </ul>
-        </Paragraph>
+        <div>
+          <Paragraph style={{ marginBottom: 20 }}>Suggestions</Paragraph>
+          <UL>
+            <LI>
+              Schedule quarterly evaluations, instead of annual. This avoids frustrations to build up over time. When an
+              employee is heard, they feel validated, which in turn leads to higher performance.
+            </LI>
+            <LI>
+              Organise remain-calls, instead of only exit-calls. Ask the employee what motivates them to stay in the
+              company, and what it would take for them to leave. This uncoves issues before they become bigger problems,
+              and allows for a focused strategy to support the employee.
+            </LI>
+          </UL>
+        </div>
       );
     } else if (likeliness < 65) {
       return (
-        <Paragraph>
-          Suggestions
-          <ul>
-            <li>This employee might feel like their accomplishments are not recognized.
-            Inspect their performance and reward when due. A reward can be a simple e-mail, card, or note to thank them
-          for their effort.</li>
-            <li>Problems with work-life balance contribute to decreases in motivation and performance. Check
-          in with the employee by sending an e-mail to ask how they are. Suggest them to take a day off if they can.</li>
-          </ul>
-        </Paragraph>
+        <div>
+          <Paragraph style={{ marginBottom: 20 }}>Suggestions</Paragraph>
+          <UL>
+            <LI>
+              This employee might feel like their accomplishments are not recognized. Inspect their performance and
+              reward when due. A reward can be a simple e-mail, card, or note to thank them for their effort.
+            </LI>
+            <LI>
+              Problems with work-life balance contribute to decreases in motivation and performance. Check in with the
+              employee by sending an e-mail to ask how they are. Suggest them to take a day off if they can.
+            </LI>
+          </UL>
+        </div>
       );
     }
+
     return (
-      <Paragraph>
-        Suggestions
-        <ul>
-          <li>Immediate follow-up is required, make sure to handle this personally and discretely. Invite them for a conversation to clarify that the company cares about their well-being, and values
-        them. </li>
-          <li>Absenteeism, sick leave, and increased presence of negative emotions all are warning signs of burn-out.
-          Psychological support can prevent the build-up of stress to the point of a burn-out or decision to leave their
-          job. This is a sensitive topic and should therefore be suggested at an appropriate time. Discuss with the
-        employee whether involvement of HR would help them as well, and suggest they take some time off.</li>
-        </ul>
-      </Paragraph>
+      <div>
+        <Paragraph style={{ marginBottom: 20 }}>Suggestions</Paragraph>
+        <UL>
+          <LI>
+            Immediate follow-up is required, make sure to handle this personally and discretely. Invite them for a
+            conversation to clarify that the company cares about their well-being, and values them.{' '}
+          </LI>
+          <LI>
+            Absenteeism, sick leave, and increased presence of negative emotions all are warning signs of burn-out.
+            Psychological support can prevent the build-up of stress to the point of a burn-out or decision to leave
+            their job. This is a sensitive topic and should therefore be suggested at an appropriate time. Discuss with
+            the employee whether involvement of HR would help them as well, and suggest they take some time off.
+          </LI>
+        </UL>
+      </div>
     );
   }
 
